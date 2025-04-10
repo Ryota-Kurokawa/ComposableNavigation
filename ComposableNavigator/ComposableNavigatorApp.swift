72142e2b130c23bbdef5f476c7a9e65601f55605
Ryota-Kurokawa <ryota-kurokawa@cybozu.co.jp>
2025-04-10 15:32:28 +0900

    Initial Commit

---
+++ ComposableNavigator/ComposableNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableNavigatorApp.swift
+//  ComposableNavigator
+//
+//  Created by ryota1582 on 2025/04/10.
+//
+
+import SwiftUI
+
+@main
+struct ComposableNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
